5137da617ab5cef17cab73ec3db5ce7d6dd08946
DEX Exchange <42585768+DEXExchange@users.noreply.github.com>
2022-02-06 20:02:12 -0500

    MILM NFT ERC721 Solidity Commit
    MILM NFT ERC721 - Gas Optimization - Reentrancy Guard - Whitelist - MaxMintTxPerWallet - Features/Functions
    
    Must flatten via IDE compiler.

---
+++ MILMNFT.sol
@@ -0,0 +1,216 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ /$$      /$$ /$$$$$$ /$$       /$$      /$$
+| $$$    /$$$|_  $$_/| $$      | $$$    /$$$
+| $$$$  /$$$$  | $$  | $$      | $$$$  /$$$$
+| $$ $$/$$ $$  | $$  | $$      | $$ $$/$$ $$
+| $$  $$$| $$  | $$  | $$      | $$  $$$| $$
+| $$\  $ | $$  | $$  | $$      | $$\  $ | $$
+| $$ \/  | $$ /$$$$$$| $$$$$$$$| $$ \/  | $$
+|__/     |__/|______/|________/|__/     |__/   
+/**
+// Forked from HashLips
+/**
+    !Disclaimer!
+    These contracts have been used to create tutorials,
+    and was created for the purpose to teach people
+    how to create smart contracts on the blockchain.
+    please review this code on your own before using any of
+    the following code for production.
+    The developer will not be responsible or liable for all loss or 
+    damage whatsoever caused by you participating in any way in the 
+    experimental code, whether putting money into the contract or 
+    using the code for your own project.
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract MILMNFT is ERC721, Ownable, ReentrancyGuard {
+  using Strings for uint256;
+  using Counters for Counters.Counter;
+
+  Counters.Counter private supply;
+
+  string public uriPrefix = "";
+  string public uriSuffix = ".json";
+  string public hiddenMetadataUri;
+  
+  uint256 public cost = 0.069 ether;
+  uint256 public maxSupply = 69;
+  uint256 public maxMintAmountPerTx = 3;
+
+  bool public paused = true;
+  bool public revealed = false;
+
+  bool public presale = true; 
+  mapping(address => bool) public whitelisted;
+  uint256 public maxPresaleMintAmount = 3;
+
+  constructor() ERC721("MILMNFT", "MILM") {
+    setHiddenMetadataUri("ipfs://QmSMqbQffhTwd3SMwuVTFHhx2aRPrUbsK7G8aMXdwNJJaC/prereveal.json");
+  }
+
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    _;
+  }
+
+  function totalSupply() public view returns (uint256) {
+    return supply.current();
+  }
+
+  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
+    require(!paused, "The contract is paused!");
+    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
+
+    if (presale) { //Murf dont allow minting if presale is set and buyer is not in whitelisted map
+        if ( !isInWhiteList(msg.sender))  {
+            revert("Buyer is not in Whitelist for Pre-Sale");
+        }
+        // //BM check if already bought 
+        if ( balanceOf(msg.sender)+_mintAmount > maxPresaleMintAmount)
+            revert("Buyer has already pre-sale minted max tokens");
+    } 
+
+    _mintLoop(msg.sender, _mintAmount);
+  }
+  
+  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
+    _mintLoop(_receiver, _mintAmount);
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+    uint256 currentTokenId = 1;
+    uint256 ownedTokenIndex = 0;
+
+    while (ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply) {
+      address currentTokenOwner = ownerOf(currentTokenId);
+
+      if (currentTokenOwner == _owner) {
+        ownedTokenIds[ownedTokenIndex] = currentTokenId;
+
+        ownedTokenIndex++;
+      }
+
+      currentTokenId++;
+    }
+
+    return ownedTokenIds;
+  }
+
+  function tokenURI(uint256 _tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(_tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
+    if (revealed == false) {
+      return hiddenMetadataUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
+        : "";
+  }
+
+  function setRevealed(bool _state) public onlyOwner {
+    revealed = _state;
+  }
+
+  function setCost(uint256 _cost) public onlyOwner {
+    cost = _cost;
+  }
+
+  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
+  }
+
+  function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
+    hiddenMetadataUri = _hiddenMetadataUri;
+  }
+
+  function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+    uriPrefix = _uriPrefix;
+  }
+
+  function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+    uriSuffix = _uriSuffix;
+  }
+
+  function setPaused(bool _state) public onlyOwner {
+    paused = _state;
+  }
+
+  function withdraw() public onlyOwner nonReentrant {
+    // This will pay MILM Suga Mama Fund 50% of the initial sale.
+    // 15% of the BAAC Genesis mint ETH revenues allocates this support to NASA's NCAS Community College.
+    // =============================================================================
+    (bool hs, ) = payable(0xCabeEe95b404435613C7d53BFBb3cD9a0dB87B53).call{value: address(this).balance * 50 / 100}("");
+    require(hs);
+    // =============================================================================
+
+    // This will transfer the remaining contract balance to the owner.
+    // Do not remove this otherwise you will not be able to withdraw the funds.
+    // =============================================================================
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+    // =============================================================================
+  }
+
+  function _mintLoop(address _receiver, uint256 _mintAmount) internal {
+    for (uint256 i = 0; i < _mintAmount; i++) {
+      supply.increment();
+      _safeMint(_receiver, supply.current());
+    }
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return uriPrefix;
+  }
+
+  //Murf Whitelist Functions
+  function setPresale(bool _state) public onlyOwner {
+      presale = _state;
+  }
+
+  function setMaxPresaleMintAmount(uint256 _max) public onlyOwner {
+      maxPresaleMintAmount = _max;
+  }
+
+  function addToWhiteList(address _addr) public onlyOwner {
+      whitelisted[_addr] = true;
+  }
+
+  function addArrayToWhiteList(address[] memory _addrs) public onlyOwner {
+      for (uint256 i=0;i< _addrs.length;i++)
+          whitelisted[_addrs[i]] = true; 
+  }
+
+  function removeFromWhiteList(address _addr) public onlyOwner {
+      whitelisted[_addr] = false;
+  }
+
+  function isInWhiteList(address _addr) private view returns (bool) {
+      return whitelisted[_addr]  || _addr == owner();
+  }
+}
